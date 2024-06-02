f2f8f360446f399ef7f9488d57e8339e3037e08d
Michael Buesch <m@bues.ch>
2024-06-02 23:24:42 +0200

    server: Simplify message processing

--- letmeind/src/processor.rs
+++ letmeind/src/processor.rs
@@ -9,7 +9,7 @@
 use crate::{firewall::Firewall, server::Connection, ConfigRef};
 use anyhow::{self as ah, format_err as err};
 use letmein_conf::Resource;
-use letmein_proto::{Challenge, Key, Message, Operation};
+use letmein_proto::{Message, Operation};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -19,9 +19,6 @@ pub struct Processor<'a> {
     fw: Arc<Mutex<Firewall>>,
     user_id: Option<u32>,
     resource_id: Option<u32>,
-    resource: Option<Resource>,
-    challenge: Option<Challenge>,
-    key: Option<Key>,
 }
 
 impl<'a> Processor<'a> {
@@ -32,9 +29,6 @@ impl<'a> Processor<'a> {
             fw,
             user_id: None,
             resource_id: None,
-            resource: None,
-            challenge: None,
-            key: None,
         }
     }
 
@@ -73,31 +67,30 @@ impl<'a> Processor<'a> {
     async fn send_go_away(&mut self) -> ah::Result<()> {
         self.send_msg(Message::new(
             Operation::GoAway,
-            self.user_id.unwrap_or(0),
-            self.resource_id.unwrap_or(0),
+            self.user_id.unwrap_or(u32::MAX),
+            self.resource_id.unwrap_or(u32::MAX),
         ))
         .await
     }
 
-    async fn handle_knock(&mut self) -> ah::Result<()> {
-        let msg = self.recv_msg(Operation::Knock).await?;
+    pub async fn run(&mut self) -> ah::Result<()> {
+        let knock = self.recv_msg(Operation::Knock).await?;
 
-        let user_id = msg.user();
+        let user_id = knock.user();
         self.user_id = Some(user_id);
 
-        let resource_id = msg.resource();
+        let resource_id = knock.resource();
         self.resource_id = Some(resource_id);
 
-        // Get the shared key and cache it.
+        // Get the shared key.
         let Some(key) = self.conf.key(user_id) else {
             let _ = self.send_go_away().await;
             return Err(err!("Unknown user: {user_id:X}"));
         };
-        self.key = Some(*key);
 
         // Authenticate the received message.
         // This check is not replay-safe. But that's fine.
-        if !msg.check_auth_ok_no_challenge(key) {
+        if !knock.check_auth_ok_no_challenge(key) {
             let _ = self.send_go_away().await;
             return Err(err!("Knock: Authentication failed"));
         }
@@ -107,57 +100,34 @@ impl<'a> Processor<'a> {
             let _ = self.send_go_away().await;
             return Err(err!("Unknown resource: {resource_id:X}"));
         };
-        self.resource = Some(resource.clone());
 
         // Generate and send a challenge.
         let mut challenge_msg = Message::new(Operation::Challenge, user_id, resource_id);
-        self.challenge = Some(challenge_msg.generate_challenge());
+        let challenge = challenge_msg.generate_challenge();
         self.send_msg(challenge_msg).await?;
 
-        Ok(())
-    }
-
-    async fn handle_challenge_response(&mut self) -> ah::Result<()> {
-        let msg = self.recv_msg(Operation::Response).await?;
+        // Receive the response.
+        let response = self.recv_msg(Operation::Response).await?;
 
         // Authenticate the challenge-response.
-        if !msg.check_auth_ok(&self.key.take().unwrap(), &self.challenge.take().unwrap()) {
+        if !response.check_auth_ok(key, &challenge) {
             let _ = self.send_go_away().await;
             return Err(err!("Response: Authentication failed"));
         }
 
-        Ok(())
-    }
-
-    async fn handle_open_firewall(&mut self) -> ah::Result<()> {
         // Reconfigure the firewall.
-        match self.resource.take().unwrap() {
+        match resource {
             Resource::Port(port) => {
                 let mut fw = self.fw.lock().await;
-                fw.open_port(self.conf, self.conn.addr().ip(), port).await?;
+                fw.open_port(self.conf, self.conn.addr().ip(), *port)
+                    .await?;
             }
         }
 
-        Ok(())
-    }
-
-    async fn handle_come_in(&mut self) -> ah::Result<()> {
         // Send a come-in message.
-        let comein_msg = Message::new(
-            Operation::ComeIn,
-            self.user_id.unwrap(),
-            self.resource_id.unwrap(),
-        );
-        self.send_msg(comein_msg).await?;
+        let comein = Message::new(Operation::ComeIn, user_id, resource_id);
+        self.send_msg(comein).await?;
 
-        Ok(())
-    }
-
-    pub async fn run(&mut self) -> ah::Result<()> {
-        self.handle_knock().await?;
-        self.handle_challenge_response().await?;
-        self.handle_open_firewall().await?;
-        self.handle_come_in().await?;
         Ok(())
     }
 }
