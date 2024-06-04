3fc89c05220478f68cacf9077fe08350e91bf69c
Michael Buesch <m@bues.ch>
2024-06-04 18:52:36 +0200

    client: Allow specifying server port on command line

--- letmein/src/main.rs
+++ letmein/src/main.rs
@@ -37,18 +37,24 @@ async fn recv_msg(client: &mut Client, expect_operation: Operation) -> ah::Resul
     Ok(reply)
 }
 
-async fn run_knock(conf: Arc<Config>, addr: &str, port: u16, user: Option<u32>) -> ah::Result<()> {
+async fn run_knock(
+    conf: Arc<Config>,
+    addr: &str,
+    server_port: Option<u16>,
+    knock_port: u16,
+    user: Option<u32>,
+) -> ah::Result<()> {
     let user = user.unwrap_or_else(|| conf.default_user());
     let Some(key) = conf.key(user) else {
         return Err(err!("No key found in letmein.conf for user {user:08X}"));
     };
-    let Some(resource) = conf.resource_id_by_port(port) else {
+    let Some(resource) = conf.resource_id_by_port(knock_port) else {
         return Err(err!(
-            "Port {port} is not mapped to a resource in letmein.conf"
+            "Port {knock_port} is not mapped to a resource in letmein.conf"
         ));
     };
 
-    let mut client = Client::new(addr, conf.port())
+    let mut client = Client::new(addr, server_port.unwrap_or_else(|| conf.port()))
         .await
         .context("Client init")?;
 
@@ -89,13 +95,38 @@ fn parse_hex(s: &str) -> ah::Result<u32> {
 #[derive(Subcommand, Debug)]
 enum Command {
     Knock {
+        /// The host name, IPv4 or IPv6 address that you want to knock on.
         host: String,
+        /// The port on the remote host that you want to knock open.
         port: u16,
 
+        /// The user identifier (8 digits hex number) to use for
+        /// authenticating this knock request.
+        /// The authentication key associated with this user identifier
+        /// will be fetched from the letmein.conf configuration file.
+        /// If not given, then the `[CLIENT] default_user` from the
+        /// configuration file will be used instead.
+        /// If the configuration is not available, user 00000000 will
+        /// be used instead.
         #[arg(long, short, value_parser = parse_hex)]
         user: Option<u32>,
+
+        /// You normally don't have to use this option.
+        /// Set the letmein server port number to use when contacting the letmein server.
+        /// If not given, then the `[GENERAL] port` from the
+        /// letmein.conf configuration file will be used instead.
+        /// If the configuration is not available, port 5800 will
+        /// be used instead.
+        #[arg(long)]
+        server_port: Option<u16>,
     },
     GenKey {
+        /// The user identifier (8 digits hex number) to use in the
+        /// generated key string.
+        /// If not given, then the `[CLIENT] default_user` from the
+        /// configuration file will be used instead.
+        /// If the configuration is not available, user 00000000 will
+        /// be used instead.
         #[arg(long, short, value_parser = parse_hex)]
         user: Option<u32>,
     },
@@ -109,7 +140,12 @@ async fn main() -> ah::Result<()> {
     );
 
     match opts.command {
-        Command::Knock { host, port, user } => run_knock(conf, &host, port, user).await?,
+        Command::Knock {
+            host,
+            port,
+            user,
+            server_port,
+        } => run_knock(conf, &host, server_port, port, user).await?,
         Command::GenKey { user } => run_genkey(conf, user).await?,
     }
 
