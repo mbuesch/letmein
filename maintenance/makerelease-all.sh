cfcdc19c37b2417c8594c669149e52641dac585a
Michael Buesch <m@bues.ch>
2024-06-09 00:06:01 +0200

    Use versions refs for deps

--- maintenance/makerelease-all.sh
+++ maintenance/makerelease-all.sh
@@ -3,8 +3,8 @@
 basedir="$(realpath "$0" | xargs dirname)"
 
 set -e
-"$basedir/makerelease-letmein.sh" "$@"
-"$basedir/makerelease-letmeind.sh" "$@"
 "$basedir/makerelease-letmein-conf.sh" "$@"
 "$basedir/makerelease-letmein-proto.sh" "$@"
 "$basedir/makerelease-letmein-systemd.sh" "$@"
+"$basedir/makerelease-letmein.sh" "$@"
+"$basedir/makerelease-letmeind.sh" "$@"
