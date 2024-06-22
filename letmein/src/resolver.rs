f88f3759f74c9db1b8f25e2410743a3f3a55782b
Michael Buesch <m@bues.ch>
2024-06-22 20:00:53 +0200

    resolver: Fall back to Google DNS, if system setup failed

--- letmein/src/resolver.rs
+++ letmein/src/resolver.rs
@@ -6,9 +6,9 @@
 // or the MIT license, at your option.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use anyhow::{self as ah, format_err as err, Context as _};
+use anyhow::{self as ah, format_err as err};
 use hickory_proto::rr::{record_data::RData, record_type::RecordType};
-use hickory_resolver::TokioAsyncResolver;
+use hickory_resolver::{config::ResolverConfig, TokioAsyncResolver};
 use std::net::IpAddr;
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
@@ -21,32 +21,49 @@ pub enum ResMode {
 pub async fn resolve(host: &str, mode: ResMode) -> ah::Result<IpAddr> {
     // Try to parse host as an IP address.
     if let Ok(addr) = host.parse::<IpAddr>() {
-        if addr.is_ipv4() && mode != ResMode::Ipv4 {
-            return Err(err!(
-                "Supplied a raw IPv4 address, but resolution mode is set to IPv6"
-            ));
+        match mode {
+            ResMode::Ipv4 if !addr.is_ipv4() => {
+                return Err(err!(
+                    "Supplied a raw IPv4 address, but resolution mode is set to IPv6"
+                ));
+            }
+            ResMode::Ipv6 if !addr.is_ipv6() => {
+                return Err(err!(
+                    "Supplied a raw IPv6 address, but resolution mode is set to IPv4"
+                ));
+            }
+            _ => (),
         }
-        if addr.is_ipv6() && mode != ResMode::Ipv6 {
-            return Err(err!(
-                "Supplied a raw IPv6 address, but resolution mode is set to IPv4"
-            ));
-        }
+        // It is an IP address. No need for DNS lookup.
         return Ok(addr);
     }
 
-    let resolver = TokioAsyncResolver::tokio_from_system_conf()
-        .context("Create address resolver from system configuration")?;
+    // Create a DNS resolver.
+    let resolver;
+    if let Ok(r) = TokioAsyncResolver::tokio_from_system_conf() {
+        resolver = r;
+    } else {
+        eprintln!(
+            "Warning: Could not create DNS resolver from system configuration. \
+             Is /etc/resolv.conf present? Falling back to Google DNS."
+        );
+        resolver = TokioAsyncResolver::tokio(ResolverConfig::google(), Default::default());
+    }
+
+    // Determine the DNS record type from the address resolution mode.
     let (record_type, record_type_str) = match mode {
         ResMode::Ipv6 => (RecordType::AAAA, "AAAA"),
         ResMode::Ipv4 => (RecordType::A, "A"),
     };
 
+    // Do a DNS lookup of the host.
     let Ok(lookup) = resolver.lookup(host, record_type).await else {
         return Err(err!(
             "DNS lookup of host '{host}' failed. No '{record_type_str}' record found."
         ));
     };
 
+    // Return the first address that matches the requested address resolution mode.
     for addr in lookup {
         match (mode, addr) {
             (ResMode::Ipv6, RData::AAAA(addr)) => return Ok(addr.0.into()),
