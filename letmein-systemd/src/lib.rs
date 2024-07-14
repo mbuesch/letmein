e7760c35a39a5bfb6802ec55d63239536a9a6890
Michael Buesch <m@bues.ch>
2024-07-14 19:52:05 +0200

    systemd: Check if the socket fd is valid

--- letmein-systemd/src/lib.rs
+++ letmein-systemd/src/lib.rs
@@ -9,23 +9,91 @@
 //! This crate is an abstraction of the `systemd` interfaces needed by `letmein`.
 
 use anyhow::{self as ah, format_err as err, Context as _};
-use std::{net::TcpListener, os::fd::FromRawFd as _};
+
+#[cfg(feature = "tcp")]
+use std::{
+    mem::size_of_val,
+    net::TcpListener,
+    os::fd::{FromRawFd as _, RawFd},
+};
+
+/// Check if the passed raw `fd` is a socket.
+#[cfg(feature = "tcp")]
+fn is_socket(fd: RawFd) -> bool {
+    let mut stat: libc::stat64 = unsafe { std::mem::zeroed() };
+    let ret = unsafe { libc::fstat64(fd, &mut stat) };
+    if ret == 0 {
+        (stat.st_mode & libc::S_IFMT) == libc::S_IFSOCK
+    } else {
+        false
+    }
+}
+
+/// Get the socket type of the passed socket `fd`.
+///
+/// SAFETY: The passed `fd` must be a socket `fd`.
+#[cfg(feature = "tcp")]
+unsafe fn get_socket_type(fd: RawFd) -> Option<libc::c_int> {
+    let mut sotype: libc::c_int = 0;
+    let mut len: libc::socklen_t = size_of_val(&sotype) as _;
+    let ret = unsafe {
+        libc::getsockopt(
+            fd,
+            libc::SOL_SOCKET,
+            libc::SO_TYPE,
+            &mut sotype as *mut _ as _,
+            &mut len,
+        )
+    };
+    if ret == 0 && len >= size_of_val(&sotype) as _ {
+        Some(sotype)
+    } else {
+        None
+    }
+}
+
+/// Get the socket family of the passed socket `fd`.
+///
+/// SAFETY: The passed `fd` must be a socket `fd`.
+#[cfg(feature = "tcp")]
+unsafe fn get_socket_family(fd: RawFd) -> Option<libc::c_int> {
+    let mut saddr: libc::sockaddr = unsafe { std::mem::zeroed() };
+    let mut len: libc::socklen_t = size_of_val(&saddr) as _;
+    let ret = unsafe { libc::getsockname(fd, &mut saddr, &mut len) };
+    if ret == 0 && len >= size_of_val(&saddr) as _ {
+        Some(saddr.sa_family.into())
+    } else {
+        None
+    }
+}
+
+#[cfg(feature = "tcp")]
+fn is_tcp_socket(fd: RawFd) -> bool {
+    let inet46 = [Some(libc::AF_INET), Some(libc::AF_INET6)];
+    // SAFETY: Check if `fd` is a socket before using the socket functions.
+    unsafe {
+        is_socket(fd)
+            && get_socket_type(fd) == Some(libc::SOCK_STREAM)
+            && inet46.contains(&get_socket_family(fd))
+    }
+}
 
 /// Create a new [TcpListener] with the socket provided by systemd.
 ///
 /// All environment variables related to this operation will be cleared.
+#[cfg(feature = "tcp")]
 pub fn tcp_from_systemd() -> ah::Result<Option<TcpListener>> {
     if sd_notify::booted().unwrap_or(false) {
-        let mut fds = sd_notify::listen_fds().context("Systemd listen_fds")?;
-        if let Some(fd) = fds.next() {
-            // SAFETY:
-            // The fd from systemd is good and lives for the lifetime of the program.
-            return Ok(Some(unsafe { TcpListener::from_raw_fd(fd) }));
-        } else {
-            return Err(err!(
-                "Booted with systemd, but no listen_fds received from systemd."
-            ));
+        for fd in sd_notify::listen_fds().context("Systemd listen_fds")? {
+            if is_tcp_socket(fd) {
+                // SAFETY:
+                // The fd from systemd is good and lives for the lifetime of the program.
+                return Ok(Some(unsafe { TcpListener::from_raw_fd(fd) }));
+            }
         }
+        return Err(err!(
+            "Booted with systemd, but no TCP listen_fds received from systemd."
+        ));
     }
     Ok(None)
 }
