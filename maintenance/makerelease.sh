22ac00dcd1628e96f6e861b5db787fadeac432b2
Michael Buesch <m@bues.ch>
2024-12-16 21:40:21 +0100

    makerelease: Run tests

--- maintenance/makerelease.sh
+++ maintenance/makerelease.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# -*- coding: utf-8 -*-
 
 srcdir="$(realpath "$0" | xargs dirname)"
 srcdir="$srcdir/.."
@@ -7,16 +8,25 @@ srcdir="$srcdir/.."
 # https://bues.ch/cgit/misc.git/tree/makerelease.lib
 die() { echo "$*"; exit 1; }
 for path in $(echo "$PATH" | tr ':' ' '); do
-	[ -f "$MAKERELEASE_LIB" ] && break
-	MAKERELEASE_LIB="$path/makerelease.lib"
+    [ -f "$MAKERELEASE_LIB" ] && break
+    MAKERELEASE_LIB="$path/makerelease.lib"
 done
 [ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
 
 hook_get_version()
 {
-	version="$(cargo_local_pkg_version letmein)"
+    version="$(cargo_local_pkg_version letmein)"
+}
+
+hook_regression_tests()
+{
+    default_hook_regression_tests "$@"
+
+    sh "$1"/tests/run-tests.sh
 }
 
 project=letmein
 conf_upload_packages="letmein-proto letmein-fwproto letmein-conf letmein-systemd letmein-seccomp letmein letmeinfwd letmeind"
 makerelease "$@"
+
+# vim: ts=4 sw=4 expandtab
