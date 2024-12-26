a8e208e423e6f320cfe0be3a4930769af2550e8a
Michael Buesch <m@bues.ch>
2024-12-26 22:17:15 +0100

    tests: Check nft command line arguments

--- tests/stubs/nft.rs
+++ tests/stubs/nft.rs
@@ -8,7 +8,21 @@
 
 #![forbid(unsafe_code)]
 
+use std::{
+    env,
+    io::{stdin, Read as _},
+};
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args, ["nft", "-j", "-f", "-"]);
+
+    let mut json_raw: Vec<u8> = vec![];
+    stdin().read_to_end(&mut json_raw).unwrap();
+    let json = std::str::from_utf8(&json_raw).unwrap();
+
+    //eprintln!("{json:?}");
+    let _ = json; // We could have some json payload checks here...
 }
 
 // vim: ts=4 sw=4 expandtab
