dda32bdc14051f054ca95225ad44ec627a46d0d9
Michael Buesch <m@bues.ch>
2024-12-26 21:19:10 +0100

    stubs: Add license header

--- tests/stubs/nft.rs
+++ tests/stubs/nft.rs
@@ -1,2 +1,14 @@
+// -*- coding: utf-8 -*-
+//
+// Copyright (C) 2024 Michael Büsch <m@bues.ch>
+//
+// Licensed under the Apache License version 2.0
+// or the MIT license, at your option.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![forbid(unsafe_code)]
+
 fn main() {
 }
+
+// vim: ts=4 sw=4 expandtab
