99544aced8813c058067a81f4c9c0d315d20071a
Michael Büsch <m@bues.ch>
2025-12-31 22:09:49 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -3,7 +3,24 @@
 
 basedir="$(realpath "$0" | xargs dirname)"
 
+[ -f "$basedir/Cargo.toml" ] || die "basedir sanity check failed"
 . "$basedir/scripts/lib.sh"
+
+release="both"
+while [ $# -ge 1 ]; do
+    case "$1" in
+        --debug|-d)
+            release="debug"
+            ;;
+        --release|-r)
+            release="release"
+            ;;
+        *)
+            die "Invalid option: $1"
+            ;;
+    esac
+    shift
+done
 
 check_dynlibs()
 {
@@ -28,24 +45,30 @@ check_dynlibs()
     done
 }
 
-[ -f "$basedir/Cargo.toml" ] || die "basedir sanity check failed"
-
 cd "$basedir" || die "cd basedir failed."
 export LETMEIN_CONF_PREFIX="/opt/letmein"
-cargo build || die "Cargo build (debug) failed."
-cargo test || die "Cargo test failed."
-if which cargo-auditable >/dev/null 2>&1; then
-    cargo auditable build --release || die "Cargo build (release) failed."
-    cargo audit --deny warnings bin \
-        target/release/letmein \
-        target/release/letmeind \
-        target/release/letmeinfwd \
-        || die "Cargo audit failed."
-else
-    cargo build --release || die "Cargo build (release) failed."
+
+# Debug build and test
+if [ "$release" = "debug" -o "$release" = "both" ]; then
+    cargo build || die "Cargo build (debug) failed."
+    cargo test || die "Cargo test failed."
 fi
-check_dynlibs target/release/letmein
-check_dynlibs target/release/letmeind
-check_dynlibs target/release/letmeinfwd
+
+# Release build
+if [ "$release" = "release" -o "$release" = "both" ]; then
+    if which cargo-auditable >/dev/null 2>&1; then
+        cargo auditable build --release || die "Cargo build (release) failed."
+        cargo audit --deny warnings bin \
+            target/release/letmein \
+            target/release/letmeind \
+            target/release/letmeinfwd \
+            || die "Cargo audit failed."
+    else
+        cargo build --release || die "Cargo build (release) failed."
+    fi
+    check_dynlibs target/release/letmein
+    check_dynlibs target/release/letmeind
+    check_dynlibs target/release/letmeinfwd
+fi
 
 # vim: ts=4 sw=4 expandtab
