0ff74342f05ae0fa517ef8349ba2400d60a03640
Michael Büsch <m@bues.ch>
2026-03-09 19:55:29 +0100

    Upgrade to sd-notify 0.5
    Remove the clearing of the NOTIFY_SOCKET environment variable,
    because that cannot safely be done at this point.
    
    For letmeind this is Ok. The seccomp rules prevent opening the socket.
    For letmeinfwd a subprocess like nft could use the socket.
    This is not a security problem because we must fully trust root nft anyway.

--- letmein-systemd/src/lib.rs
+++ letmein-systemd/src/lib.rs
@@ -188,10 +188,8 @@ impl SystemdSocket {
 }
 
 /// Notify ready-status to systemd.
-///
-/// All environment variables related to this operation will be cleared.
 pub fn systemd_notify_ready() -> ah::Result<()> {
-    sd_notify::notify(true, &[sd_notify::NotifyState::Ready])?;
+    sd_notify::notify(&[sd_notify::NotifyState::Ready])?;
     Ok(())
 }
 
